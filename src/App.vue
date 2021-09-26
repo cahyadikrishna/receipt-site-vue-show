981c32c858471cd495c70d62a837a92573fe3de9
dipadana <dipadanadev@gmail.com>
2021-09-26 16:01:20 +0800

    Update vite setting and add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,16 @@
 <script>
-import { ref } from "vue";
-import Card from "./components/Card.vue";
-import Footer from "./components/Footer.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  components: { Card, Footer },
+  components: { Footer },
   setup() {
-    const categoryData = ref([]);
-
-    async function getData() {
-      const response = await fetch(
-        "https://www.themealdb.com/api/json/v1/1/categories.php"
-      );
-      const payload = await response.json();
-      categoryData.value = payload.categories;
-    }
-
-    getData();
-
-    return { Card, Footer, categoryData };
+    return { Footer };
   },
 };
 </script>
 
 <template>
-  <div class="container d-flex flex-column justify-content-center">
-    <!-- Search bar -->
-    <p class="text-center fs-1 fw-bold text-success mt-5">Receipt Site</p>
-
-    <div class="row mt-2 mb-4">
-      <div class="col-10">
-        <input
-          class="form-control form-control-lg"
-          type="text"
-          placeholder="Search here..."
-        />
-      </div>
-      <div class="col-2">
-        <button type="button" class="btn btn-outline-success btn-lg w-100">
-          🔍 Search
-        </button>
-      </div>
-    </div>
-
-    <!-- Card component list -->
-    <div class="row row-cols-1 row-cols-md-5 g-4 mt-3">
-      <Card
-        v-for="data in categoryData"
-        :key="data.idCategory"
-        :category="data.strCategory"
-        :description="data.strCategoryDescription"
-        :image="data.strCategoryThumb"
-      />
-    </div>
-
-    <!-- Footer component -->
-    <Footer />
-  </div>
+  <router-view />
+  <!-- Footer component -->
+  <Footer />
 </template>
-
-<style></style>
